29d72b1386a4de92e0858612b4e782fc0475a0e8
Hironobu Matsuhisa <matsuhisa@gmail.com>
2015-05-05 13:18:35 +0900

    マイグレーションを実装してみた

--- TestAppCocoaPods01/Categories.swift
+++ TestAppCocoaPods01/Categories.swift
@@ -7,6 +7,9 @@ import CoreData
 class Categories: NSManagedObject {
 
     @NSManaged var name: String
+    @NSManaged var description_text: String?
+    @NSManaged var created_at: NSDate?
+    @NSManaged var updated_at: NSDate?
     
     func name_plus() -> NSString{
         var neme_plus = self.name + "だよ"
--- TestAppCocoaPods01/ViewController.swift
+++ TestAppCocoaPods01/ViewController.swift
@@ -8,7 +8,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        initDataInsert()
+        //initDataInsert()
         readData()
 
         if let category: Categories = Categories.by("name", equalTo: "foo").find().firstObject() as? Categories {
@@ -32,7 +32,10 @@ class ViewController: UIViewController {
         
         for(var i = 0; i < items.count; i++) {
             var categories = items[i] as! Categories
+            println("---")
             println(categories.name_plus())
+            println(categories.description_text)
+            println("---")
         }
         
         println("------------")
@@ -40,15 +43,18 @@ class ViewController: UIViewController {
     
     // データーの登録
     func initDataInsert() {
-        let category_names: [String] = ["日記", "写真", "技術", "映画", "foo", "bar"]
+        let category_names: [String] = ["日記", "写真", "技術", "映画", "foo", "bar", "foobar", "zoo"]
         for category_name in category_names {
             println("------------")
             println(category_name)
-            var categories:Categories = Categories.create() as! Categories
-            categories.beginWriting()
-            categories.name = category_name
-            var saved:Bool  = categories.save()
-            categories.endWriting()
+
+            var item:Categories = Categories.create() as! Categories
+            item.name = category_name
+            item.description_text = category_name + "の説明文です"
+            item.created_at = NSDate()
+            item.updated_at = NSDate()
+            var saved:Bool  = item.save()
+
             println(saved)
             println("------------")
         }
